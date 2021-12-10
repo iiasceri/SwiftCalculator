07a06ee7ed6c5c92ea07880de90a39317a53e2fb
Nick <ion.iascerinschi@gmail.com>
2021-12-10 01:23:47 +0000

    First commit, buttons are not working yet

---
+++ AutoLayout-Calculator/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  AutoLayout-Calculator
+//
+//  Created by Ion Iascerinschi on 8/21/18.
+//  Copyright © 2018 Ion Iascerinschi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonClearAllPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonErasePressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonNumberPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonOperatorPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonEqualsPressed(_ sender: Any) {
+        
+    }
+    
+}
+
